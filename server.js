22775e15b1c838bfdaaee053be45b3040905c26c
billymartinezlasso27-commits <billymartinezlasso27@gmail.com>
2026-03-05 19:10:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const { PeerServer } = require('peer');
-
 const peerServer = PeerServer({
   port: process.env.PORT || 9000,
-  path: '/',
+  path: '/peerjs',
   allow_discovery: true,
   proxied: true
 });
-
 peerServer.on('connection', (client) => {
   console.log('Client connected:', client.getId());
 });
-
 peerServer.on('disconnect', (client) => {
   console.log('Client disconnected:', client.getId());
 });
-
 console.log('PeerJS server running on port', process.env.PORT || 9000);
